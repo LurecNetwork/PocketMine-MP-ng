96f359233dca421720255bda5d3fca866c04a30c
imlucky <167329163+imLuckii@users.noreply.github.com>
2024-09-17 17:18:44 +0000

    Just me playing around

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ AQUA='\033[0;36m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+START_TIME_TO_CALCULATE=$(date)
 if [ -d "bin" ]; then
     echo -e "${RED}Removing current bin directory...${NC}"
     rm -rf bin > /dev/null 2>&1
@@ -16,11 +17,15 @@ echo -e "${AQUA}Installing dependencies...${NC}"
 /workspaces/PocketMine-MP-ng/bin/php7/bin/php /usr/local/bin/composer install --no-dev --classmap-authoritative > /dev/null 2>&1
 
 if [ -f "PocketMine-MP.phar" ]; then
-    echo -e "${RED}Removing current PocketMine-MP.phar...${NC}"
-    rm -f PocketMine-MP.phar > /dev/null 2>&1
+    echo -e "${RED}Renaming and moving current PocketMine-MP.phar...${NC}"
+    OLD_VERSIOn_NAME=$(date +"%d-%m-%y")
+    mv PocketMine-MP.phar old_builds/PocketMine-MP_${OLD_VERSIOn_NAME}.phar
 fi
 
 echo -e "${AQUA}Making new PocketMine-MP.phar...${NC}"
 composer make-server > /dev/null 2>&1
 
-echo -e "${AQUA}The new PocketMine-MP.phar has been created!${NC}"
+# Calculate time, including milliseconds (ex: 4.543 seconds)
+TIME_TAKE_TO_CALCULATE=$(($(date +%s%N) - $(date -d "$START_TIME_TO_CALCULATE" +%s%N)))
+TIME_TAKE_TO_CALCULATE=$(echo "scale=3; $TIME_TAKE_TO_CALCULATE / 1000000000" | bc)
+echo -e "${AQUA}PocketMine-MP.phar has been built in ${TIME_TAKE_TO_CALCULATE}s${NC}"
